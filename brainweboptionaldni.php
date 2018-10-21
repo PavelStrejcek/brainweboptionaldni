c2ec1162e9fe519044d370d44efdc420a3c6561d
Paul <pavel.strejcek@brainweb.cz>
2018-10-21 23:07:02 +0200

    support for PrestaShop 1.6.1.21-22

--- brainweboptionaldni.php
+++ brainweboptionaldni.php
@@ -30,10 +30,10 @@ class BrainWebOptionalDni extends Module
 	{
 		$this->name = 'brainweboptionaldni';
 		$this->tab = 'others';
-		$this->version = '1.0.14';
+		$this->version = '1.0.15';
 		$this->author = 'Pavel Strejček [BrainWeb.cz]';
 		$this->need_instance = 1;
-		$this->ps_versions_compliancy = array('min' => '1.6.1.2', 'max' => '1.6.1.20');
+		$this->ps_versions_compliancy = array('min' => '1.6.1.2', 'max' => '1.6.1.22');
 		$this->bootstrap = true;
 
 		parent::__construct();
